62d5bdbd6f4772ba322666759ce01b87a7bbcb60
vedb1211 <106091820+vedb1211@users.noreply.github.com>
2023-03-27 10:47:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ def calculate():
     bmi = ''
     children = ''
     smoking = ''
+    gender = ''
+    gender_female = ''
+    gender_male= ''
     insurance = ''
 
     if request.method == 'POST' and 'age' in request.form and 'bmi' in request.form:
@@ -15,11 +18,21 @@ def calculate():
         bmi = request.form.get('bmi')
         children = request.form.get('children')
         smoking = request.form.get('smoking')
+        gender = request.form.get('gender')
+        if smoking=='yes' or smoking=='Yes' or smoking=='YES':
+            smoking_yes=1
+        elif smoking=='no' or smoking=='No'or smoking=='NO':
+            smoking_yes=0
+        if gender=='male' or gender=='Male' or gender=='MALE': 
+            gender_male=1
+            gender_female=0
+        elif gender=='female' or gender=='Female' or gender=='FEMALE': 
+            gender_male=0
+            gender_female=1
 
-        insurance = round(8167.9  + (3194.18 * float(age)) + (1086.33 * float(bmi)) + (311.49 * int(children)) + (25050 * int(smoking)),2)
+        insurance = round(5633.51+(3715.28*int(age))+(1999.61*int(bmi))+(651.17*int(children))+(23530*int(smoking_yes)+2616.04*int(gender_male)+3017.46*int(gender_female)),2)
 
-    return render_template("index.html", age=age, bmi=bmi, children=children, smoking=smoking, insurance=insurance)
+    return render_template("index.html", age=age, bmi=bmi, children=children, smoking=smoking,gender=gender,insurance=insurance)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
